b007dfdb359a5252cbe259b49e55af7fdd7b7d68
wolfgray <5565486@mail.ru>
2023-03-21 19:26:10 +0300

    fix temlate margins

--- script.js
+++ script.js
@@ -2,18 +2,21 @@ const background = {
   avia: "./img/A3.png",
   avto: "img/A2.png",
   sea: "img/A10.png",
-  track: "img/A4.png",
+  train: "img/A4.png",
   import: "img/A6.png",
 };
 
 const info = document.querySelector(".info-container");
 info.style.backgroundImage = `url(${background.avia})`;
-// for (let el in background) {
-//   setInterval(() => {
-//     console.log(background[el]);
-//     info.style.background =`url(${background[el]})`
-//   }, 1500);
-// }
+const switcher = document.querySelectorAll("input[type=radio]");
+
+for (let el of switcher) {
+  el.addEventListener('change', ()=> {
+info.style.backgroundImage = `url(${background[el.id]})`;
+
+  })
+}
+
 const menuBtn = document.querySelectorAll(".btn");
 menuBtn[1].classList.add("checked");
 for (let el of menuBtn) {
